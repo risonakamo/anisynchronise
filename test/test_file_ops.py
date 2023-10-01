29b1f47e2c1f5e0a6badecac2ff4d2badddc4970
risonakamo <hmykanae@gmail.com>
2023-10-01 14:08:56 -0400

    mirror copy functions

---
+++ test/test_file_ops.py
@@ -0,0 +1,7 @@
+from anisynchronise.file_ops import mirrorCopy
+
+
+mirrorCopy(
+    r"C:\Users\ngokn1\Desktop\wark\old stuff\air\airui readme",
+    r"C:\Users\ngokn1\Desktop\wark\old stuff\air\aligntest"
+)
